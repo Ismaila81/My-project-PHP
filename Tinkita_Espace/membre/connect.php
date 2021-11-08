09037bfe84e27b92ea7b6ff6e0602320b753952d
Ismaila_Tinkita <89797092+Ismaila81@users.noreply.github.com>
2021-11-08 13:53:54 +0000

    Add files via upload

---
+++ Tinkita_Espace/membre/connect.php
@@ -0,0 +1,8 @@
+<?php
+/*remplacer les valeurs suivantes par les valeurs
+ exactes*/
+define ('SERVEUR', "localhost");
+define ('LOGIN', "Tinkita");
+define ('MDP', "Tinkita2025");
+define ('BDD', "Visiteurs");
+?>
